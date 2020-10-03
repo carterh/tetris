768e6c839f5cc8fba04de23ba6913cef36be8087
Hank Carter <carterh@gatech.edu>
2020-10-03 17:19:41 -0400

    Finished first pass at the game drawing and controls setup

--- lib/main.py
+++ lib/main.py
@@ -4,25 +4,63 @@ import sys
 import engine
 
 #Constants
-size = width, height = (300, 600)
-black = 0,0,0
+#make sure the size and board dimensions are the correct ratio, i.e. 2w = h
+SIZE = WIDTH, HEIGHT = (300, 600)
+BOARD = BWIDTH, BHEIGHT = (10, 20)
+square = swidth, sheight = (WIDTH//BWIDTH, HEIGHT//BHEIGHT)
+square_fill = fwidth, fheight = (WIDTH//BWIDTH - 1, HEIGHT//BHEIGHT - 1)
+#line_buffer = 2
+BACKGROUND_COLOR = pygame.Color('blue')
+GAME_TICK = pygame.USEREVENT
+BORDER_WIDTH = 5
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('Tetris at home')
+def color_piece(piece_value):
+    if piece_value == 0:
+        return pygame.Color('blue')
+    elif piece_value == 1:
+        return pygame.Color('red')
+    else:
+        return pygame.Color('green')
 
+def convert_rc_to_coords(row, col):
+    y = row*sheight
+    x = col*swidth
+    return x, y
+
+def draw_piece(row, col, value, canvas):
+    x, y = convert_rc_to_coords(row, col)
+    block = pygame.Rect(x,y,fwidth, fheight)
+    border = pygame.draw.rect(canvas, pygame.Color('black'), block, BORDER_WIDTH)
+    canvas.fill(color_piece(value), rect=border)
+
+def draw_game(screen, state):
     background = pygame.Surface(screen.get_size())
-    #what does this do?
     background = background.convert()
-    background.fill(black)
+    background.fill(BACKGROUND_COLOR)
+    
+    for row in range(len(state)):
+        for col in range(len(state[row])):
+            piece_value = state[row][col]
+            if piece_value:
+                draw_piece(row, col, piece_value, background)
 
-    #Is this necessary after the fill call?
     screen.blit(background, (0,0))
     pygame.display.flip()
 
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(SIZE)
+    pygame.display.set_caption('Tetris at home')
+    
+    draw_game(screen, engine.static_game())
+
+    # If you want to change the speed, set this timer back to 0 and create a new one in the game loop (maybe a SPEED_UP event?)
+    pygame.time.set_timer(GAME_TICK, 1000)
     playing = True
     while playing:
+
+        # If too many events are happening before the draw happens, I may need to convert this to a 
+        # pygame.event.poll(), handle one event, then redraw
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 playing = False
@@ -33,7 +71,12 @@ def main():
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
                 print('right')
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                print('space')
+                print('rotate')
+            elif event.type == GAME_TICK:
+                print('tick')
+        
+        draw_game(screen, engine.static_game())
+    
     print(str(engine.static_game()))
     pygame.quit()
 
